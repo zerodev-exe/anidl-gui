778b4801b7495f3603c83f93646d8a195a0d3d87
Zero <zerodev.exe@proton.me>
2024-09-10 22:38:48 -0400

    Added a notification when a download is completed

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -27,7 +27,7 @@ async fn search_anime(name: &str) -> Result<Vec<AnimeInfo>, String> {
             img_url,
         })
         .collect();
-    Ok(anime_list   )
+    Ok(anime_list)
 }
 
 #[tauri::command]
@@ -38,6 +38,14 @@ async fn download_anime(anime_url_ending: &str, anime_name: &str) -> Result<(), 
     get_anime_episodes_and_download_the_episodes(anime_url_ending.to_string(), anime_name)
         .await
         .unwrap();
+
+    // Send notification
+    tauri::api::notification::Notification::new("Download Complete")
+        .title("Download Complete")
+        .body(format!("The download of {} is complete!", anime_name))
+        .show()
+        .map_err(|e| e.to_string())?;
+
     Ok(())
 }
 
@@ -79,7 +87,14 @@ async fn set_filter_sub(is_sub: bool) -> Result<(), String> {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![search_anime, download_anime, get_filter_dub, set_filter_dub, get_filter_sub, set_filter_sub])
+        .invoke_handler(tauri::generate_handler![
+            search_anime,
+            download_anime,
+            get_filter_dub,
+            set_filter_dub,
+            get_filter_sub,
+            set_filter_sub
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
