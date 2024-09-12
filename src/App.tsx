ff13d198c2d06dfca71d44130695c6fa98b97dc8
Zero <zerodev.exe@proton.me>
2024-09-12 14:09:04 -0400

    Changed the name and added some icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Search from './Search';
+import z from '/public/z.svg';
 
 function App() {
 	return (
 		<div className="container">
+			<img className='logo vite' src={z} alt="Zero's Anime Downloader" />
 			<h1>Zero's Anime Downloader</h1>
 			<Search />
 		</div>
