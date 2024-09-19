81be89c5feb82b8edfdba28253953ce7dc18e754
Zero <zerodev.exe@proton.me>
2024-09-18 21:04:06 -0400

    Put all of the settings stuff in another file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,10 @@
 
 mod download;
 mod scraper;
+mod settings;
 use gogoanime_scraper::get_anime_list_by_name;
 use scraper::get_anime_episodes_and_download_the_episodes;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
@@ -18,36 +19,6 @@ lazy_static::lazy_static! {
 fn update_download_progress(file_path: &str, progress: f64) {
     let mut progress_map = DOWNLOAD_PROGRESS.lock().unwrap();
     progress_map.insert(file_path.to_string(), progress);
-}
-
-#[derive(Serialize, Deserialize)]
-struct Settings {
-    filter_dub: bool,
-    filter_sub: bool,
-}
-
-fn get_settings_path() -> std::path::PathBuf {
-    let home_dir = dirs::home_dir().expect("Could not find home directory");
-    home_dir.join("settings.toml")
-}
-
-fn load_settings() -> Result<Settings, String> {
-    let path = get_settings_path();
-    if path.exists() {
-        let content = fs::read_to_string(path).map_err(|e| e.to_string())?;
-        toml::de::from_str(&content).map_err(|e| e.to_string())
-    } else {
-        Ok(Settings {
-            filter_dub: false,
-            filter_sub: false,
-        }) // Default values
-    }
-}
-
-fn save_settings(settings: &Settings) -> Result<(), String> {
-    let path = get_settings_path();
-    let content = toml::ser::to_string(settings).map_err(|e| e.to_string())?;
-    fs::write(path, content).map_err(|e| e.to_string())
 }
 
 #[derive(Serialize)]
@@ -94,32 +65,6 @@ async fn download_anime(anime_url_ending: &str, anime_name: &str) -> Result<(), 
 }
 
 #[tauri::command]
-async fn get_filter_dub() -> Result<bool, String> {
-    let settings = load_settings()?;
-    Ok(settings.filter_dub)
-}
-
-#[tauri::command]
-async fn set_filter_dub(is_dub: bool) -> Result<(), String> {
-    let mut settings = load_settings()?;
-    settings.filter_dub = is_dub;
-    save_settings(&settings)
-}
-
-#[tauri::command]
-async fn get_filter_sub() -> Result<bool, String> {
-    let settings = load_settings()?;
-    Ok(settings.filter_sub)
-}
-
-#[tauri::command]
-async fn set_filter_sub(is_sub: bool) -> Result<(), String> {
-    let mut settings = load_settings()?;
-    settings.filter_sub = is_sub;
-    save_settings(&settings)
-}
-
-#[tauri::command]
 async fn check_downloads() -> Result<serde_json::Value, String> {
     let anime_dir = dirs::video_dir().unwrap().join("Anime");
 
@@ -160,7 +105,7 @@ async fn check_downloads() -> Result<serde_json::Value, String> {
                         downloaded.push(folder_name.to_string_lossy().into_owned());
                         progress.insert(
                             folder_name.to_string_lossy().into_owned(),
-                            (downloaded_episodes, downloaded_episodes),
+                            (downloaded_episodes, downloaded_episodes.try_into().unwrap()),
                         ); // Use downloaded_episodes as total_episodes
                     }
                 }
@@ -208,10 +153,10 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             search_anime,
             download_anime,
-            get_filter_dub,
-            set_filter_dub,
-            get_filter_sub,
-            set_filter_sub,
+            settings::get_filter_dub,
+            settings::set_filter_dub,
+            settings::get_filter_sub,
+            settings::set_filter_sub,
             check_downloads
         ])
         .run(tauri::generate_context!())
---
+++ src-tauri/src/settings.rs
@@ -0,0 +1,59 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+use toml;
+
+#[derive(Serialize, Deserialize)]
+struct Settings {
+    filter_dub: bool,
+    filter_sub: bool,
+}
+
+fn get_settings_path() -> std::path::PathBuf {
+    let home_dir = dirs::home_dir().expect("Could not find home directory");
+    home_dir.join("settings.toml")
+}
+
+fn load_settings() -> Result<Settings, String> {
+    let path = get_settings_path();
+    if path.exists() {
+        let content = fs::read_to_string(path).map_err(|e| e.to_string())?;
+        toml::de::from_str(&content).map_err(|e| e.to_string())
+    } else {
+        Ok(Settings {
+            filter_dub: false,
+            filter_sub: false,
+        }) // Default values
+    }
+}
+
+fn save_settings(settings: &Settings) -> Result<(), String> {
+    let path = get_settings_path();
+    let content = toml::ser::to_string(settings).map_err(|e| e.to_string())?;
+    fs::write(path, content).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub async fn get_filter_dub() -> Result<bool, String> {
+    let settings = load_settings()?;
+    Ok(settings.filter_dub)
+}
+
+#[tauri::command]
+pub async fn set_filter_dub(is_dub: bool) -> Result<(), String> {
+    let mut settings = load_settings()?;
+    settings.filter_dub = is_dub;
+    save_settings(&settings)
+}
+
+#[tauri::command]
+pub async fn get_filter_sub() -> Result<bool, String> {
+    let settings = load_settings()?;
+    Ok(settings.filter_sub)
+}
+
+#[tauri::command]
+pub async fn set_filter_sub(is_sub: bool) -> Result<(), String> {
+    let mut settings = load_settings()?;
+    settings.filter_sub = is_sub;
+    save_settings(&settings)
+}
