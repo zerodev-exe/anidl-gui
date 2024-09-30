37b53eacdc2d720ec0c2687889075149fe334dde
Zero <zerodev.exe@proton.me>
2024-09-29 23:32:12 -0400

    Changed the check for update button to the settings tab

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import About from "../src/About";
 import Download from "../src/Download";
 import Settings from "../src/Settings";
 import "./Header.css";
-import { checkForUpdates } from '../components/utils/Update';
 
 
 function Header() {
@@ -26,7 +25,6 @@ function Header() {
               <Link to="/about" className="nav-link">
                 <li className="nav-item">About</li>
               </Link>
-			<button onClick={checkForUpdates}>Check for Updates</button>
 
             </ul>
           </nav>
--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,5 +1,9 @@
+import { checkForUpdates } from '../components/utils/Update';
+
 function Settings() {
-    return <div></div>;
+    return <div>
+        <button onClick={checkForUpdates}>Check for Updates</button>
+    </div>;
 }
 
 export default Settings;
