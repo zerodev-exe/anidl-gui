c6288a4a5764e4656455926a38d0a831f306b77c
Zero <zerodev.exe@proton.me>
2024-10-03 10:32:01 -0400

    It was giving me an error (it isn't anymore)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Search from '../components/Search';
-import z from '/z.svg';
 import "./App.css";
 
 function App() {
 	return (
 		<div className="container">
-			<img className='logo vite' src={z} alt="Zero's Anime Downloader" />
+			<img className="logo vite" src="/z.svg" alt="Zero's Anime Downloader" />
 			<h1>Zero's Anime Downloader</h1>
 			<Search />
 		</div>
